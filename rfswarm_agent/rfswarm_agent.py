cc95e2c9bf8ad1d21dd87918bae31be04e4d6a45
Dave Amies <damies13@gmail.com>
2020-12-28 11:07:28 +1000

    Base function, just need to ping server now

--- rfswarm_agent/rfswarm_agent.py
+++ rfswarm_agent/rfswarm_agent.py
@@ -165,6 +165,9 @@ class RFSwarmAgent():
 			self.debugmsg(6, "self.xmlmode: ", self.xmlmode)
 			self.create_listner_file()
 
+		t = threading.Thread(target=self.tick_counter)
+		t.start()
+
 
 	def debugmsg(self, lvl, *msg):
 		msglst = []
@@ -358,6 +361,17 @@ class RFSwarmAgent():
 		else:
 			self.config['Agent']['swarmserver'] = "http://localhost:8138/"
 			self.saveini()
+
+	def tick_counter(self):
+
+		# t = threading.Thread(target=self.tick_counter)
+		# t.start()
+		# only tick once per day
+		# 1 day, 24 hours  = 60 * 60 * 24
+		aday = 60 * 60 * 24
+		while True:
+			self.debugmsg(9, "tick_counter")
+			time.sleep(aday)
 
 
 	def getscripts(self):
