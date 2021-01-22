7562b5dc5eb6015ddfc18254cdcbe189fd91969f
Dave Amies <damies13@gmail.com>
2021-01-22 20:40:26 +1000

    fix minor bugs in reporting %ile
    - if there were fail's (None type result) in the result set the %ile would fail to calculate
    - used to not calculate if count under 10, while this worked it's not valid for some %ile's like 96%-99% where a higher minimum is required and for others like 75% and 80% a %ile could be calculated with as low as 4 or 5 results receptively, now the minimum needed is calculated and the %ile is returned when the result count is greater than the minimum needed for that %ile

--- rfswarm_gui/rfswarm.py
+++ rfswarm_gui/rfswarm.py
@@ -65,16 +65,20 @@ class percentile:
 		self.values = []
 
 	def step(self, value, percent):
+		base.debugmsg(9, "value:", value, "	percent:", percent)
+		if value is None:
+			return
 		self.count += 1
 		self.values.append(value)
 		self.percent = percent
 
 	def finalize(self):
 		try:
-			if self.count <10:
+			mincount = 100/(100-self.percent)
+			if self.count < mincount:
 				# Need at least 10 samples to get a useful percentile
 				return None
-			base.debugmsg(9, "percentile: finalize: self.count:", self.count, "	self.percent:", self.percent, "	self.values:", self.values)
+			base.debugmsg(9, "percentile: finalize: mincount:", mincount, "	self.count:", self.count, "	self.percent:", self.percent, "	self.values:", self.values)
 			nth = self.count * (self.percent/100)
 			base.debugmsg(9, "percentile: finalize: nth:", nth)
 			nthi = int(nth)
