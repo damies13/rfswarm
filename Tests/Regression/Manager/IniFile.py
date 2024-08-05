de7e11d6e9031b1daacbf09d845b9c3877f2f7d7
Arkadiusz Kuczynski <arkadiusz.kuczynski@niceproject.eu>
2024-08-05 23:51:20 +0200

    Trying to get super linter happy

--- Tests/Regression/Manager/IniFile.py
+++ Tests/Regression/Manager/IniFile.py
@@ -1,14 +1,17 @@
+"""Module providing a function reading ini type file
+and returns dictionary with the content of the file."""
+
 import configparser
 
 
-class IniFile:
-	def read_ini_file(self, filename):
-		config = configparser.ConfigParser()
-		config.read(filename)
+def read_ini_file(filename):
+	"Read ini file and return dictionary."
+	config = configparser.ConfigParser()
+	config.read(filename)
 
-		variables = {}
-		for section in config.sections():
-			variables[section] = {}
-			for key, value in config.items(section):
-				variables[section][key] = value
-		return variables
+	variables = {}
+	for section in config.sections():
+		variables[section] = {}
+		for key, value in config.items(section):
+			variables[section][key] = value
+	return variables
