fad72ac67aaaaab85479167508c4563a9c0f3a5e
Arkadiusz Kuczynski <arkadiusz.kuczynski@niceproject.eu>
2024-08-05 23:06:56 +0200

    Trying to make super linter happy

--- Tests/Regression/Manager/IniFile.py
+++ Tests/Regression/Manager/IniFile.py
@@ -1,14 +1,13 @@
 import configparser
 
 
-class IniFile:
-	def read_ini_file(self, filename):
-		config = configparser.ConfigParser()
-		config.read(filename)
+def read_ini_file(filename):
+	config = configparser.ConfigParser()
+	config.read(filename)
 
-		variables = {}
-		for section in config.sections():
-			variables[section] = {}
-			for key, value in config.items(section):
-				variables[section][key] = value
-		return variables
+	variables = {}
+	for section in config.sections():
+		variables[section] = {}
+		for key, value in config.items(section):
+			variables[section][key] = value
+	return variables
