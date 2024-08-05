7ba9c1ab29fe2233d128f4f241c47c4422dbbb92
Arkadiusz Kuczynski <arkadiusz.kuczynski@niceproject.eu>
2024-08-05 23:26:07 +0200

    Checking something

--- Tests/Regression/Manager/IniFile.py
+++ Tests/Regression/Manager/IniFile.py
@@ -1,13 +1,14 @@
 import configparser
 
 
-def read_ini_file(filename):
-	config = configparser.ConfigParser()
-	config.read(filename)
+class IniFile:
+	def read_ini_file(self, filename):
+		config = configparser.ConfigParser()
+		config.read(filename)
 
-	variables = {}
-	for section in config.sections():
-		variables[section] = {}
-		for key, value in config.items(section):
-			variables[section][key] = value
-	return variables
+		variables = {}
+		for section in config.sections():
+			variables[section] = {}
+			for key, value in config.items(section):
+				variables[section][key] = value
+		return variables
