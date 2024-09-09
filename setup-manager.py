de87829e82e330d51d1cb345b8799a7f7c8ff7b3
damies13 <damies13@gmail.com>
2024-09-09 22:13:30 +1000

    try gui_scripts instead of console_scripts Issue #145

--- setup-manager.py
+++ setup-manager.py
@@ -48,7 +48,10 @@ setup(
 		'Say Thanks!': 'https://github.com/damies13/rfswarm#donations',
 		'Source': 'https://github.com/damies13/rfswarm',
 	},
-	entry_points={'console_scripts': ['rfswarm = rfswarm_manager.rfswarm:RFSwarm', 'rfswarm-manager = rfswarm_manager.rfswarm:RFSwarm']},
+	# https://stackoverflow.com/questions/68747660/is-there-any-way-to-have-icon-for-exe-file-that-created-as-an-entry-point-using
+	# entry_points={'console_scripts': ['rfswarm = rfswarm_manager.rfswarm:RFSwarm', 'rfswarm-manager = rfswarm_manager.rfswarm:RFSwarm']},
+	# entry_points={'console_scripts': ['rfswarm = rfswarm_manager.rfswarm:RFSwarm', 'rfswarm-manager = rfswarm_manager.rfswarm:RFSwarm'], 'gui_scripts': ['rfswarm-manager-gui = rfswarm_manager.rfswarm:RFSwarm']},
+	entry_points={'gui_scripts': ['rfswarm = rfswarm_manager.rfswarm:RFSwarm', 'rfswarm-manager = rfswarm_manager.rfswarm:RFSwarm']},
 )
 
 # https://pypi.org/project/pyshortcuts/
