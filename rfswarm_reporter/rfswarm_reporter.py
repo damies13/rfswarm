6e0a875212bb962f68fa937af7dcbece9f09d40e
damies13 <damies13@gmail.com>
2024-09-21 01:08:18 +1000

    Update version to 1.4.0

--- rfswarm_reporter/rfswarm_reporter.py
+++ rfswarm_reporter/rfswarm_reporter.py
@@ -3105,7 +3105,7 @@ class ReporterCore:
 
 		Infoplist = os.path.join(apppath, "Contents", "Info.plist")
 		with open(Infoplist, "w") as f:
-			f.write("""<?xml version = "1.4.0"8"?>
+			f.write("""<?xml version = "1.4.0""?>
 			<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 			<plist version = "1.4.0"
 			<dict>
