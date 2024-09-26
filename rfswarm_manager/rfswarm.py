eadd0ba9b8cca0a068934889557fe2e0fbf63c99
damies13 <damies13@gmail.com>
2024-09-27 07:20:56 +1000

    Update version to 1.4.0

--- rfswarm_manager/rfswarm.py
+++ rfswarm_manager/rfswarm.py
@@ -2923,7 +2923,7 @@ class RFSwarmCore:
 
 		Infoplist = os.path.join(apppath, "Contents", "Info.plist")
 		with open(Infoplist, "w") as f:
-			f.write("""<?xml version = "1.4.0"="UTF-8"?>
+			f.write("""<?xml version = "1.4.0""UTF-8"?>
 			<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 			<plist version = "1.4.0"
 			<dict>
