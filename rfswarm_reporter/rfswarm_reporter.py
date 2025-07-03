bf58eac30d686c68668afc841a9b90538fe282ab
damies13 <damies13@gmail.com>
2025-07-03 16:09:11 +1000

    Stage 1 for Reporter Issue #318

--- rfswarm_reporter/rfswarm_reporter.py
+++ rfswarm_reporter/rfswarm_reporter.py
@@ -63,76 +63,14 @@ from matplotlib.figure import Figure  # required for matplot graphs
 # required for company logo's (I beleive this is a depandancy of matplotlib anyway)
 from PIL import Image, ImageTk
 
+if True:  # noqa: E402
+	sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+	from percentile import percentile
+	from stdevclass import stdevclass
+
 matplotlib.use("TkAgg") 	# required for matplot graphs
 
 __name__ = "rfswarm-manager"
-
-
-class percentile:
-	count = 0
-	percent = 90
-	values: Any = []
-
-	def __init__(self):
-		self.count = 0
-		self.percent = 90
-		self.values = []
-
-	def step(self, value, percent):
-		base.debugmsg(9, "value:", value, "	percent:", percent)
-		if value is None:
-			return
-		self.count += 1
-		self.values.append(value)
-		self.percent = percent
-
-	def finalize(self):
-		try:
-			mincount = 100 / (100 - self.percent)
-			if self.count < mincount:
-				# Need at least 10 samples to get a useful percentile
-				return None
-			base.debugmsg(9, "percentile: finalize: mincount:", mincount, "	self.count:", self.count, "	self.percent:", self.percent, "	self.values:", self.values)
-			nth = self.count * (self.percent / 100)
-			base.debugmsg(9, "percentile: finalize: nth:", nth)
-			nthi = int(nth)
-			# nthi = int(math.ceil(self.count * (self.percent/100)))
-			self.values.sort()
-			base.debugmsg(8, "percentile: finalize: nthi:", nthi, "	self.values[nthi]:", self.values[nthi], "	self.values:", self.values)
-			return self.values[nthi]
-			# return self.count
-		except Exception as e:
-			base.debugmsg(5, "Exception:", e)
-
-
-class stdevclass:
-	M = 0.0
-	S = 0.0
-	k = 1
-
-	def __init__(self):
-		self.M = 0.0
-		self.S = 0.0
-		self.k = 1
-
-	def step(self, value):
-		if value is None:
-			return
-		tM = self.M
-		self.M += (value - tM) / self.k
-		self.S += (value - tM) * (value - self.M)
-		self.k += 1
-
-	def finalize(self):
-		base.debugmsg(9, "self.k:", self.k, "	self.S:", self.S, "	self.M:", self.M)
-		if self.k < 3:
-			return None
-		try:
-			res = math.sqrt(self.S / (self.k - 2))
-			base.debugmsg(8, "res:", res)
-			return res
-		except Exception as e:
-			base.debugmsg(5, "Exception:", e)
 
 
 class ReporterBase:
