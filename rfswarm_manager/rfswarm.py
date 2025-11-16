57f5c4ec8c6178c0159ce3179c8d4fcc4ba6f924
damies13 <damies13@gmail.com>
2025-11-16 15:15:44 +1000

    Handle the file not exist exception nicely Issue #396

--- rfswarm_manager/rfswarm.py
+++ rfswarm_manager/rfswarm.py
@@ -1747,8 +1747,13 @@ class RFSwarmBase:
 		base.debugmsg(6, "checkhashes:", checkhashes)
 		for chkhash in checkhashes:
 			file_data = self.scriptfiles[chkhash]
-			script_hash = base.hash_file(file_data['localpath'], file_data['relpath'])
-			if script_hash != chkhash:
+			try:
+				script_hash = base.hash_file(file_data['localpath'], file_data['relpath'])
+			except:
+				script_hash = None
+				self.remove_hash(chkhash)
+
+			if script_hash is not None and script_hash != chkhash:
 				# file changed
 				base.debugmsg(3, "File's hash has changed: chkhash:", chkhash, "	script_hash:", script_hash, "	localpath:", file_data['localpath'])
 
